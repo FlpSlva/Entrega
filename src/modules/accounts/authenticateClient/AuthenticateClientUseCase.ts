a01c738c8ac038f1df28f2ec88f8553aa8fa77a6
FlpSlva <flpslvadev@gmail.com>
2022-07-04 21:26:27 -0300

    criando authenticateClientUseCase

---
+++ src/modules/accounts/authenticateClient/AuthenticateClientUseCase.ts
@@ -0,0 +1,37 @@
+import { compare } from "bcrypt"
+import { sign } from "jsonwebtoken"
+import {prisma} from "../../database/prismaClient"
+
+interface IAuthenticateClient {
+    username: string
+    password: string
+}
+
+class AuthenticateClientUseCase {
+    async execute({password, username}: IAuthenticateClient){
+        
+    const client = await prisma.clients.findFirst({
+        where:{
+            username
+        }
+    })
+
+    if(!client) throw new Error("Username or Password invalid !")
+
+    const passwordMatch = await compare(password, client.password);
+
+    if(!passwordMatch) throw new Error("Username or Password invalid !")
+
+
+    const token = sign({username}, "698dc19d489c4e4db73e28a713eab07b", {
+        subject: client.id,
+        expiresIn: "1d"
+    })
+
+
+    return token;
+
+
+
+    }
+}
