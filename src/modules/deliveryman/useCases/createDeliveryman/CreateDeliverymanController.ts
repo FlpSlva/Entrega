56ac6c339d30eeeb75e8011c56abcfaafe6359ff
FlpSlva <flpslvadev@gmail.com>
2022-07-11 19:16:07 -0300

    criando o controller de deliveryman

---
+++ src/modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from "express";
+import { CreateDeliverymanUseCase } from "./CreateDeliverymanUseCase";
+
+
+
+
+class CreateDeliverymanController{
+
+    async handle(request: Request, response: Response){
+        const {username, password} = request.body;
+        const createDeliverymanUseCase = new CreateDeliverymanUseCase();
+        const result = await createDeliverymanUseCase.execute({
+            username,
+            password
+        })
+
+         if(!result) throw new Error;
+
+        return response.status(201).json({result});
+    }
+
+}
+
+
+export {CreateDeliverymanController};
